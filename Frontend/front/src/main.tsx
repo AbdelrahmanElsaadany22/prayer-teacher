98c49e461162612e695eebae03905628be040762
abdallah7absa <abdallah7absa@gmail.com>
2026-06-20 00:39:36 +0300

    msAa

--- Frontend/front/src/main.tsx
+++ Frontend/front/src/main.tsx
@@ -6,6 +6,8 @@ import { LanguageProvider } from './shared/i18n/LanguageProvider';
 import './index.css';
 import { router } from './router.tsx';
 
+#hi
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <LanguageProvider>
